abeb7df501bb8561c455ccddef82daa2da118bb6
843366055@qq.com <843366055@qq.com>
2017-07-30 23:22:00 +0800

    ....

--- app/src/main/java/com/example/administrator/pandatv/ui/pandaeye/PandaEyeFragment.java
+++ app/src/main/java/com/example/administrator/pandatv/ui/pandaeye/PandaEyeFragment.java
@@ -1,11 +1,16 @@
 package com.example.administrator.pandatv.ui.pandaeye;
 
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.administrator.pandatv.R;
 import com.example.administrator.pandatv.base.BaseFragment;
 import com.example.administrator.pandatv.entity.PandaEyeBean;
+import com.example.administrator.pandatv.net.HttpFactroy;
 import com.jcodecraeer.xrecyclerview.XRecyclerView;
+
+import java.util.List;
 
 import butterknife.BindView;
 
@@ -16,6 +21,8 @@ import butterknife.BindView;
 public class PandaEyeFragment extends BaseFragment implements PandaEyeContract.View {
     @BindView(R.id.pandaeye_xRecyclerview)
     XRecyclerView pandaeyeXRecyclerview;
+    private ImageView pandaeye_img;
+    private TextView pandaeye_title;
 
 
     @Override
@@ -25,7 +32,10 @@ public class PandaEyeFragment extends BaseFragment implements PandaEyeContract.V
 
     @Override
     protected void init(View view) {
-
+        View head = View.inflate(getContext(), R.layout.pandaeye_item, null);
+        pandaeye_img = (ImageView) head.findViewById(R.id.pandaeye_img);
+        pandaeye_title = (TextView) head.findViewById(R.id.pandaeye_title);
+        pandaeyeXRecyclerview.addHeaderView(head);
     }
 
     @Override
@@ -35,7 +45,9 @@ public class PandaEyeFragment extends BaseFragment implements PandaEyeContract.V
 
     @Override
     public void showPandaBean(PandaEyeBean pandaEyeBean) {
-
+        List<PandaEyeBean.DataBean.BigImgBean> bigImg = pandaEyeBean.getData().getBigImg();
+        HttpFactroy.create().loadImage(bigImg.get(0).getImage(),pandaeye_img);
+        pandaeye_title.setText(bigImg.get(0).getTitle());
     }
 
     @Override
