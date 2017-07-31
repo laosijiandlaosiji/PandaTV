dbf5453a368b938d11d6f2644a7c34c16dc798b1
843366055@qq.com <843366055@qq.com>
2017-07-31 09:52:25 +0800

    ..........111

---
+++ app/src/main/java/com/example/administrator/pandatv/widget/view/BannerImageLoader.java
@@ -0,0 +1,18 @@
+package com.example.administrator.pandatv.widget.view;
+
+import android.content.Context;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.youth.banner.loader.ImageLoader;
+
+/**
+ * Created by Administrator on 2017/7/30.
+ */
+
+public class BannerImageLoader extends ImageLoader {
+    @Override
+    public void displayImage(Context context, Object path, ImageView imageView) {
+        Glide.with(context).load(path).into(imageView);
+    }
+}
